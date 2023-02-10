9e892fb84ed90d95b226f6f18173d2ac7d3b2e56
Eugen Lange <lange.e@mail.ru>
2023-02-11 00:31:21 +0500

    1-2 задание

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,28 @@
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        List<Integer> nums = new ArrayList<>(List.of(1, 1, 2, 3, 4, 4, 5, 5, 6, 7));
+        System.out.println(nums);
+        List<Integer> oddNums = new ArrayList<>();
+        for (int i = 0; i < nums.size(); i++) {
+            if (nums.get(i) % 2 == 1) {
+                oddNums.add(nums.get(i));
+            }
+
+        }
+        System.out.println(oddNums);
+        HashSet<Integer> evenNums = new HashSet<>();
+        for (int i = 0; i < nums.size(); i++) {
+            if (nums.get(i) % 2 == 0) {
+                evenNums.add(nums.get(i));
+            }
+            
+        }
+        System.out.println(evenNums);
+
     }
+
 }
