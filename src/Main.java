f682da389bf55c0b3d024e9727b1c1d7c5bd4389
Eugen Lange <lange.e@mail.ru>
2023-02-11 15:22:26 +0500

    3 задание в работе

--- src/Main.java
+++ src/Main.java
@@ -5,7 +5,7 @@ import java.util.List;
 public class Main {
     public static void main(String[] args) {
         List<Integer> nums = new ArrayList<>(List.of(1, 1, 2, 3, 4, 4, 5, 5, 6, 7));
-        System.out.println(nums);
+        System.out.println("Исходная последовательность: " + nums);
         List<Integer> oddNums = new ArrayList<>();
         for (int i = 0; i < nums.size(); i++) {
             if (nums.get(i) % 2 == 1) {
@@ -13,15 +13,19 @@ public class Main {
             }
 
         }
-        System.out.println(oddNums);
+        System.out.println("Нечетные числа из последовательности: " + oddNums);
         HashSet<Integer> evenNums = new HashSet<>();
         for (int i = 0; i < nums.size(); i++) {
             if (nums.get(i) % 2 == 0) {
                 evenNums.add(nums.get(i));
             }
-            
+
         }
-        System.out.println(evenNums);
+        System.out.println("Нечетные числа из последовательности, без повторов: " +evenNums);
+        String text = "На дворе — трава, на траве — дрова. Не руби дрова на траве двора!";
+        System.out.println(text);
+        List<String> wordsFromText = List.of(text.split(".[^[a-zA-ZА-Яа-я]["-"]]"?i));
+        System.out.println(wordsFromText);
 
     }
 
