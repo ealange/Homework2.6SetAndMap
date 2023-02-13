9d849de13da8d8cddbb0b574647aacb799217af1
Eugen Lange <lange.e@mail.ru>
2023-02-13 23:17:12 +0500

    Завершено

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,4 @@
-import java.util.ArrayList;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Locale;
+import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
@@ -22,23 +19,114 @@ public class Main {
             }
 
         }
-        System.out.println("Нечетные числа из последовательности, без повторов: " +evenNums);
+        System.out.println("Нечетные числа из последовательности, без повторов: " + evenNums);
 
-        String text = "На дворе — трава, на траве — дрова. Не руби дрова на траве двора! Что-нибудь да как-то";
-        text = text.toLowerCase(Locale.ROOT);
-        System.out.println(text);
-        String textWithoutPunct = text.replaceAll("[,.:;!?—]", "");
-        System.out.println(textWithoutPunct);
-        String textWithoutDoubledSpaces = textWithoutPunct.replaceAll("  ", " ");
-        System.out.println(textWithoutDoubledSpaces);
-        List<String> wordsFromText = List.of(textWithoutDoubledSpaces.split("\\s"));
-        System.out.println(wordsFromText);
+        String textTask3 = "На дворе — трава, на траве — дрова. Не руби дрова на траве двора! Что-нибудь да как-то";
+        String textTask3Modified = textModify(textTask3);
+        System.out.println(textModify(textTask3Modified));
+        List<String> wordsFromText3 = List.of(textTask3Modified.split("\\s"));
+        System.out.println(wordsFromText3);
         HashSet<String> uniqueWords = new HashSet<>();
-        for (String s : wordsFromText) {
+        for (String s : wordsFromText3) {
             uniqueWords.add(s);
-
         }
         System.out.println("в тексте " + uniqueWords.size() + " уникальных слов: " + uniqueWords);
+        String houseJack = "Дом, который построил Джек\n" +
+                "Вот дом,\n" +
+                "Который построил Джек.\n" +
+                "\n" +
+                "А это пшеница,\n" +
+                "Которая в тёмном чулане хранится\n" +
+                "В доме,\n" +
+                "Который построил Джек.\n" +
+                "\n" +
+                "А это весёлая птица-синица,\n" +
+                "Которая часто ворует пшеницу,\n" +
+                "Которая в тёмном чулане хранится\n" +
+                "В доме,\n" +
+                "Который построил Джек.\n" +
+                "\n" +
+                "Вот кот,\n" +
+                "Который пугает и ловит синицу,\n" +
+                "Которая часто ворует пшеницу,\n" +
+                "Которая в тёмном чулане хранится\n" +
+                "В доме,\n" +
+                "Который построил Джек.\n" +
+                "\n" +
+                "Вот пёс без хвоста,\n" +
+                "Который за шиворот треплет кота,\n" +
+                "Который пугает и ловит синицу,\n" +
+                "Которая часто ворует пшеницу,\n" +
+                "Которая в тёмном чулане хранится\n" +
+                "В доме,\n" +
+                "Который построил Джек.\n" +
+                "\n" +
+                "А это корова безрогая,\n" +
+                "Лягнувшая старого пса без хвоста,\n" +
+                "Который за шиворот треплет кота,\n" +
+                "Который пугает и ловит синицу,\n" +
+                "Которая часто ворует пшеницу,\n" +
+                "Которая в тёмном чулане хранится\n" +
+                "В доме,\n" +
+                "Который построил Джек.\n" +
+                "\n" +
+                "А это старушка, седая и строгая,\n" +
+                "Которая доит корову безрогую,\n" +
+                "Лягнувшую старого пса без хвоста,\n" +
+                "Который за шиворот треплет кота,\n" +
+                "Который пугает и ловит синицу,\n" +
+                "Которая часто ворует пшеницу,\n" +
+                "Которая в тёмном чулане хранится\n" +
+                "В доме,\n" +
+                "Который построил Джек.\n" +
+                "\n" +
+                "А это ленивый и толстый пастух,\n" +
+                "Который бранится с коровницей строгою,\n" +
+                "Которая доит корову безрогую,\n" +
+                "Лягнувшую старого пса без хвоста,\n" +
+                "Который за шиворот треплет кота,\n" +
+                "Который пугает и ловит синицу,\n" +
+                "Которая часто ворует пшеницу,\n" +
+                "Которая в тёмном чулане хранится\n" +
+                "В доме,\n" +
+                "Который построил Джек.\n" +
+                "\n" +
+                "Вот два петуха,\n" +
+                "Которые будят того пастуха,\n" +
+                "Который бранится с коровницей строгою,\n" +
+                "Которая доит корову безрогую,\n" +
+                "Лягнувшую старого пса без хвоста,\n" +
+                "Который за шиворот треплет кота,\n" +
+                "Который пугает и ловит синицу,\n" +
+                "Которая часто ворует пшеницу,\n" +
+                "Которая в тёмном чулане хранится\n" +
+                "В доме,\n" +
+                "Который построил Джек.";
+        String houseJackModified = textModify(houseJack);
+        System.out.println(houseJackModified);
+        List<String> wordsFromText4 = List.of(houseJackModified.split("\\s"));
+        HashMap<String, Integer> wordsCount = new HashMap<>();
+        for (String s : wordsFromText4) {
+            if (wordsCount.containsKey(s)) {
+                int increaseValue = wordsCount.get(s) + 1;
+                wordsCount.put(s, increaseValue);
+            } else {
+                wordsCount.put(s, 1);
+            }
+        }
+        System.out.println("Число повторений слов в тексте: ");
+        System.out.println(wordsCount);
+    }
+
+    public static String textModify(String text) {
+        String textModify = text.toLowerCase(Locale.ROOT)
+                .replaceAll("\n", ", ")
+                .replaceAll("[,.:;!?—]", "")
+                // Замена всех двойных пробелов на одинарные
+                .replaceAll("  ", " ")
+                .replaceAll("  ", " ");
+        return textModify;
+
     }
 
 }
